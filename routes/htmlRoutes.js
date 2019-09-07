c8bfa9623313bfae3d1c8105504e620636b1fb6b
Ben Wilson <benjamin.j.wilson1990@gmail.com>
2019-09-07 11:07:47 -0500

    scraper working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,19 +7,12 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     axios.get("https://nintendonews.com/news/amiibo").then(function(response) {
       const $ = cheerio.load(response.data);
-
-      // NOT SURE IF THIS IS HOW YOU WOULD SELECT THIS DIV
       $("div.item").each(function(i, element) {
         const result = {};
-
-        // NOT SURE IF THIS IS HOW I SELECT THE TITLE
-        // result.title = $(this).find("div.info").children("h2").children("a.external.target-url").text();
         result.title = $(this).find("div.info h2 a").text();
-
         result.link = $(this).find("div.info h2 a").attr("href");
-        console.log(result);
+        // console.log(result);
         result.summary = $(this).find(".heading").children("p").text();
-
         db.Article.create(result)
       })
     })
